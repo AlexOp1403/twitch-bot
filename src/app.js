10ed8632e0f84ca4c47fbd1770f2bcc51e3fdee9
alex.opoku <alex.opoku@sinnerschrader.com>
2022-05-24 17:49:01 +0200

    quiz setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import tmi from 'tmi.js';
+import {driverQuiz, iRacingQuiz, teamsQuiz, tracksQuiz} from './quiz.js';
 import settings from "../settings.json";
+import cller from "tmi.js/lib/commands";
 
 const client = new tmi.Client({
   options: { debug: true },
@@ -14,8 +16,39 @@ const client = new tmi.Client({
   channels: settings.channels,
 });
 
-client.connect();
+client.connect().catch(console.error);
 client.on('message', (channel, userstate, message, self) => {
+  let questionsAnsered = false;
+  let pointsAdded = 0;
+  let quizStarted = false;
   if (self) return;
   client.say(channel, `@${userstate.username}, 'Hello!'`);
+  if(!quizStarted) {
+    if (message === "!driverQuiz" || quizStarted) {
+      quizStarted = true;
+      pointsAdded = driverQuiz(message);
+    }
+    if (message === "!teamsQuiz" || quizStarted) {
+      quizStarted = true;
+      pointsAdded = teamsQuiz(message)
+    }
+    if (message === "!tracksQuiz" || quizStarted) {
+      quizStarted = true;
+      pointsAdded = tracksQuiz(message)
+    }
+    if (message === "!irQuiz" || quizStarted) {
+      quizStarted = true;
+      pointsAdded = iRacingQuiz(message)
+    }
+    if (pointsAdded !== 0) {
+      questionsAnsered = true;
+    }
+  }
+  if(questionsAnsered){
+    client.say(channel, `@${userstate.username}, 'GG!'`);
+    client.say(channel,`"!add points @${userstate.username} @${pointsAdded}"` )
+    questionsAnsered = false;
+    pointsAdded = 0;
+    quizStarted = false;
+  }
 });
---
+++ src/quiz.js
@@ -0,0 +1,18 @@
+import questions from "../questions.json";
+export let currentQuestion = "";
+
+export function driverQuiz(){
+
+}
+
+export function teamsQuiz(){
+
+}
+
+export function tracksQuiz(){
+
+}
+
+export function iRacingQuiz(){
+
+}
