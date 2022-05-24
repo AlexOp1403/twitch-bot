60973944f761d05e82662a7dca7253810451be67
Alex Opoku <alex.opoku@outlook.de>
2022-05-25 00:23:40 +0200

    rewrite question logic

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import tmi from 'tmi.js';
-import {driverQuiz, iRacingQuiz, teamsQuiz, tracksQuiz} from './quiz.js';
+import {questionQuiz, questionAnswered} from './quiz.js';
 import settings from "../settings.json";
+
 let currentChannel;
 let driverQuizStarted = false;
 let teamsQuizStarted = false;
 let tracksQuizStarted = false;
 let iRacingQuizStarted = false;
-let questionsAnswered = false;
 let pointsAdded = 0;
 let quizStarted = false;
+
 export const client = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -29,39 +30,21 @@ client.on('message', (channel, userstate, message, self) => {
   currentChannel = channel;
   if (self) return;
 
-  if (message === "!driverQuiz" && !quizStarted) {
+  if (message === "!quiz" && !quizStarted) {
     botMessage("Das Quiz startet!")
-    driverQuizStarted = true;
+    quizStarted = true;
   }
-  console.log(driverQuizStarted)
-  if(driverQuizStarted){
-    driverQuiz(message);
+  if(quizStarted){
+    questionQuiz(message);
   }
 
-  if (message === "!teamsQuiz" && !quizStarted) {
-    botMessage("Das Quiz startet!")
-    teamsQuizStarted = true;
-  }
-  if(teamsQuizStarted)  tracksQuiz(message)
-
-  if (message === "!tracksQuiz" && !quizStarted) {
-    botMessage("Das Quiz startet!")
-    tracksQuizStarted = true;
-  }
-  if(tracksQuizStarted)  tracksQuiz(message)
-
-  if (message === "!irQuiz" && !quizStarted) {
-    botMessage("Das Quiz startet!")
-    iRacingQuizStarted = true;
-  }
-  if(iRacingQuizStarted)  iRacingQuiz(message)
-
-  if(questionsAnswered){
+  if(questionAnswered){
     client.say(channel, `@${userstate.username}, 'GG!'`);
     client.say(channel,`"!addPoints @${userstate.username} @${pointsAdded}"` )
-    questionsAnswered = false;
+    questionAnswered = false;
     pointsAdded = 0;
     quizStarted = false;
+
   }
 });
 
--- src/quiz.js
+++ src/quiz.js
@@ -1,31 +1,15 @@
 import questions from "../questions.json";
-import {botMessage, client} from "./app";
+import {botMessage} from "./app";
+
 export let currentQuestion;
-let questionAnswered = false;
 let quizStarted = false;
-export function driverQuiz(userMessage){
+export let questionAnswered;
+
+export function questionQuiz(userMessage){
     if(!quizStarted){
-        currentQuestion = questions.driver[Math.floor(Math.random()*questions.driver.length)]
+        currentQuestion = questions.quiz[Math.floor(Math.random()*questions.quiz.length)]
         quiz(currentQuestion)
     }
-    scanMessage(userMessage)
-}
-
-export function teamsQuiz(userMessage){
-    currentQuestion = questions.teams[Math.random()*questions.length]
-    if(!quizStarted) quiz(currentQuestion)
-    scanMessage(userMessage)
-}
-
-export function tracksQuiz(userMessage){
-    currentQuestion = questions.tracks[Math.random()*questions.length]
-    if(!quizStarted) quiz(currentQuestion)
-    scanMessage(userMessage)
-}
-
-export function iRacingQuiz(userMessage){
-    currentQuestion = questions.iRacing[Math.random()*questions.length]
-    if(!quizStarted) quiz(currentQuestion)
     scanMessage(userMessage)
 }
 
@@ -33,43 +17,35 @@ function scanMessage(message){
     if(message.toLowerCase() === currentQuestion.answer.toLowerCase()) {
         questionAnswered = true
         botMessage(`Korrekt: Es ist ${currentQuestion.answer}`)
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+        clearTimeout(timer3);
+        clearTimeout(timer4);
     }
 }
+
+let timer1;
+let timer2;
+let timer3;
+let timer4;
 
 function quiz(){
+    timer1 = undefined;
+    timer2 = undefined;
+    timer3 = undefined;
+    timer4 = undefined;
     quizStarted = true
-    console.log(currentQuestion)
     botMessage(`${currentQuestion.question}`);
-    console.log("antwort: " + questionAnswered)
-    let timer1;
-    let timer2;
-    let timer3;
-    let timer4;
-    if (!questionAnswered) {
-        timer1 = setTimeout(function () {
-            botMessage(`Tipp 1: ${currentQuestion.hint1}`);
-        }, 5000);
-    }
-    if (!questionAnswered) {
-        timer2 = setTimeout(function () {
-            botMessage(`Tipp 2: ${currentQuestion.hint2}`);
-        }, 10000);
-    }
-    if (!questionAnswered) {
-        timer3 = setTimeout(function () {
-            botMessage(`Tipp 3: ${currentQuestion.hint3}`);
-        }, 15000);
-    }
-    if (!questionAnswered) {
-        timer4 = setTimeout(function () {
-            botMessage(`Lösung: Es ist ${currentQuestion.answer}`);
-        }, 20000);
-    }
-}
-
-if(questionAnswered) {
-    clearTimeout(timer1);
-    clearTimeout(timer2);
-    clearTimeout(timer3);
-    clearTimeout(timer4);
+    timer1 = setTimeout(function () {
+        botMessage(`Tipp 1: ${currentQuestion.hint1}`);
+    }, 5000);
+    timer2 = setTimeout(function () {
+        botMessage(`Tipp 2: ${currentQuestion.hint2}`);
+    }, 10000);
+    timer3 = setTimeout(function () {
+        botMessage(`Tipp 3: ${currentQuestion.hint3}`);
+    }, 15000);
+    timer4 = setTimeout(function () {
+        botMessage(`Lösung: Es ist ${currentQuestion.answer}`);
+    }, 20000);
 }
